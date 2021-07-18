f6d9680d9fd13d7908f084b0070138044172facf
BarnaAlin <barnaalin@yahoo.com>
2021-07-18 21:28:54 +0300

    -Using a reference parameter

--- TicTacToe/TicTacToeGame.cs
+++ TicTacToe/TicTacToeGame.cs
@@ -4,9 +4,36 @@ namespace TicTacToe
 {
     public class TicTacToeGame
     {
+        int[,] board = new int[Ct.BoardSize, Ct.BoardSize];
+        int currentPlayer;
         public void Start()
         {
-            Console.WriteLine("Cica am pornit un joc");
+            InitBoard();
+            currentPlayer = Ct.FirstPlayer;
+        }
+        public void Move(int x, int y)
+        {
+            if (board[x, y] == Ct.FreeCell)
+            {
+                board[x, y] = currentPlayer;
+                TogglePlayer(ref currentPlayer);
+            }
+        }
+
+        private void TogglePlayer(ref int currentPlayer)
+        {
+            currentPlayer = (currentPlayer == Ct.FirstPlayer) ? Ct.SecondPlayer : Ct.FirstPlayer;
+        }
+
+        private void InitBoard()
+        {
+            for (int i = 0; i < Ct.BoardSize; i++)
+            {
+                for (int j = 0; j < Ct.BoardSize; j++)
+                {
+                    board[i, j] = Ct.FreeCell;
+                }
+            }
         }
     }
 }
