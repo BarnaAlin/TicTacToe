43d743e9160a1b5257980dada8d5a4750f9f4943
BarnaAlin <barnaalin@yahoo.com>
2021-07-18 21:45:38 +0300

    -Initialization of the game bord from the form

--- TicTacToe/Ct.cs
+++ TicTacToe/Ct.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace TicTacToe
 {
@@ -13,6 +14,13 @@ namespace TicTacToe
         public static int FreeCell = 0;
         public static int FirstPlayer = 1;
         public static int SecondPlayer = 2;
+        public static int NoWinner = FreeCell;
 
+        public static int LeftMargin = 20;
+        public static int TopMargin = 20;
+        public static int CellSize = 50;
+        public static string FreeCellText = ".";
+        public static int CellGap = 4;
+        public static Color CellColor = Color.LightBlue;
     }
 }
