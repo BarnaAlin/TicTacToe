96242476bdef2a41650b0f5871b396bf3f215e37
BarnaAlin <barnaalin@yahoo.com>
2021-07-18 20:55:18 +0300

    Add project files.

---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class mainForm : Form
+    {
+        TicTacToeGame game;
+        public mainForm()
+        {
+            InitializeComponent();
+            game = new TicTacToeGame();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            game.Start();
+        }
+    }
+}
